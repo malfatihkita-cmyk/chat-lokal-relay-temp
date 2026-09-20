647d0f0770d0c2d4b8decf2abf391e75060d7ee0
malfatihkita-cmyk <m.alfatih.kita@gmail.com>
2026-09-20 09:53:44 +0700

    Inspect Chrome Apple Events menu item state

--- relay/control_exec.py
+++ relay/control_exec.py
@@ -265,6 +265,41 @@ end tell
                 "stdout":p.stdout[-20000:],"stderr":p.stderr[-8000:]}
 
 
+    if a=="chrome_js_menu_item_state":
+        script=r'''
+tell application "Google Chrome" to activate
+delay 0.5
+tell application "System Events"
+ tell process "Google Chrome"
+  set viewItem to menu bar item "Lihat" of menu bar 1
+  click viewItem
+  delay 0.3
+  set devItem to menu item "Pengembang" of menu 1 of viewItem
+  set targetItem to menu item "Izinkan JavaScript dari Apple Events" of menu 1 of devItem
+  set en to enabled of targetItem
+  set mk to ""
+  try
+   set mk to value of attribute "AXMenuItemMarkChar" of targetItem
+  end try
+  set posText to ""
+  set sizeText to ""
+  try
+   set p to position of targetItem
+   set posText to (item 1 of p as text) & "," & (item 2 of p as text)
+  end try
+  try
+   set s to size of targetItem
+   set sizeText to (item 1 of s as text) & "," & (item 2 of s as text)
+  end try
+  key code 53
+  return "enabled=" & en & "|mark=" & mk & "|pos=" & posText & "|size=" & sizeText
+ end tell
+end tell
+'''
+        p=run(["/usr/bin/osascript","-e",script],45)
+        return {"ok":p.returncode==0,"returncode":p.returncode,
+                "stdout":p.stdout[-8000:],"stderr":p.stderr[-8000:]}
+
     if a=="enable_chrome_js_apple_events":
         script=r'''
 tell application "Google Chrome" to activate
